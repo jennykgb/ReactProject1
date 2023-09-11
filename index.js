410fc18aa01e644ae447cdb2c9657327e735f8ce
jennykgb <jennykgb@gmail.com>
2023-09-11 16:50:22 -0400

    Finish project

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function ReactInfo(){
+    return(
+        <div>
+            <img src="./react-logo.png" width="40px" />
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <ReactInfo />
+    
+, document.getElementById("root"))
